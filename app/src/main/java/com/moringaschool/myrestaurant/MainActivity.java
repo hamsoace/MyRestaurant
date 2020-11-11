4a9570769adcc8181496c82f4bdf3f304959e535
hamsoace <hamsoace@gmail.com>
2020-11-11 12:49:09 +0300

    changed onclick function

--- app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.myrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
