a4e9754faecec9de8bf1cf93784ffda26b2a8236
hamsoace <hamsoace@gmail.com>
2020-11-12 22:18:25 +0300

    restaurant activity oncreate method works

--- app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
+//    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
 
@@ -21,14 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
     }
+
 }
--- app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
@@ -2,13 +2,19 @@ package com.moringaschool.myrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
 }
