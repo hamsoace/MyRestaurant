de03445a8bd336dbde2b53d333c0170d0d1f96c9
hamsoace <hamsoace@gmail.com>
2020-11-13 10:53:09 +0300

    tests

---
+++ app/src/test/java/com/moringaschool/myrestaurant/MainActivityTest.java
@@ -0,0 +1,27 @@
+//package com.moringaschool.myrestaurant;
+//
+//import android.widget.TextView;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricTestRunner;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//@RunWith(RobolectricTestRunner.class)
+//    public class MainActivityTest{
+//        private MainActivity activity;
+//
+//        @Before
+//        public void setup(){
+//            activity = Robolectric.setupActivity(MainActivity.class);
+//        }
+//
+//        @Test
+//        public void validateTextViewContent(){
+//            TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+//            assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+//        }
+//}
