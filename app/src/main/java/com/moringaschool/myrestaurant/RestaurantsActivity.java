a222cf3a57338b7380ccd64e5f09d0a7a281e89b
hamsoace <hamsoace@gmail.com>
2020-11-14 18:59:15 +0300

    espresso testing passed

--- app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
@@ -13,6 +13,7 @@ public class RestaurantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
