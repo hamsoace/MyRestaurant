63867710ed82a0d23dfc8b19f91b5f69efdfec7a
hamsoace <hamsoace@gmail.com>
2020-11-14 23:40:56 +0300

    list items working

--- app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
@@ -4,16 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private String[] restaurants = new String[]{"Mi Mero Mole", "Mother's Bistro",
+        "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+        "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+        "Lardo", "Portland City Grill", "Fat Head's Brewery",
+        "Chipotle", "Subway"};
     private TextView mLocationTextView;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
