e416cbc8c74497000d5e850532d10f0f1fda5323
hamsoace <hamsoace@gmail.com>
2020-11-28 00:23:42 +0300

    Set Pager

--- app/src/main/java/com/moringaschool/myrestaurant/ui/RestaurantDetailActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/ui/RestaurantDetailActivity.java
@@ -1,15 +1,39 @@
 package com.moringaschool.myrestaurant.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
 import com.moringaschool.myrestaurant.R;
+import com.moringaschool.myrestaurant.adapters.RestaurantPagerAdapter;
+import com.moringaschool.myrestaurant.models.Business;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    private RestaurantPagerAdapter adappterViewPager;
+    List<Business> mRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+
+        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adappterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mRestaurants);
+        mViewPager.setAdapter(adappterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
