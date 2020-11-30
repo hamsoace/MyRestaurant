e3568d08a78b07a7c5b2a36f9c99dcd386f470e1
hamsoace <hamsoace@gmail.com>
2020-11-30 22:54:25 +0300

    saving to shared preference

--- app/src/main/java/com/moringaschool/myrestaurant/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/ui/MainActivity.java
@@ -2,23 +2,30 @@ package com.moringaschool.myrestaurant.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.moringaschool.myrestaurant.Constants;
 import com.moringaschool.myrestaurant.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 //    public static final String TAG = MainActivity.class.getSimpleName();
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @BindView(R.id.locationEditText) EditText mLocationEditText;
-    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
 
 
@@ -26,17 +33,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        Context context;
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        mFindRestaurantsButton.setOnClickListener(this);
+
+    }
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, RestaurantsListActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
+                if (v == mFindRestaurantsButton){
+                    String location = mLocationEditText.getText().toString();
+                    addToSharedPreferences(location);//takes user input as an argument
+                    Intent intent = new Intent(MainActivity.this, RestaurantsListActivity.class);
+                    startActivity(intent);
+        }
+    }
+    //calls editor to write this information to shared preferences
+    private void addToSharedPreferences(String location){
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();//apply() used to save this info
     }
 
 }
